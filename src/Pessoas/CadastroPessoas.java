7cb72d15998a76de7c0bfaa574d82a792ee5bdba
Lucas Repinaldo <lucasrepinaldo0811@gmail.com>
2025-05-01 09:54:58 -0300

    Enviando o pacote referente a pessoas e seus cadastros

---
+++ src/Pessoas/CadastroPessoas.java
@@ -0,0 +1,16 @@
+package Pessoas;
+
+import java.util.Scanner;
+
+public class CadastroPessoas {
+
+    private String primeiroNome;
+    private String ultimoNome;
+    private String CPF;
+
+    public CadastroPessoas(String primeiroNome, String ultimoNome, String CPF) {
+        this.primeiroNome = primeiroNome;
+        this.ultimoNome = ultimoNome;
+        this.CPF = CPF;
+    }
+}
---
+++ src/Pessoas/Services/CadastroService.java
@@ -0,0 +1,8 @@
+package Pessoas.Services;
+
+import Pessoas.CadastroPessoas;
+
+public interface CadastroService {
+
+    CadastroPessoas cadastrar();
+}
